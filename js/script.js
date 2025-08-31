a8a1f5310e6de38bf994c71370b1b4cce31ba35a
nuelo <akalo.emmanuel18@gmail.com>
2025-08-31 11:09:34 +0100

    feat: implement fixed nav bar scroll for easy navigation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 "use strick";
 
 const hamburgerBtn = document.getElementById("hamburger-btn");
-const hams = document.querySelectorAll(".ham");
-const mobileNav = document.getElementById('mobile-nav')
+const mobileNav = document.getElementById("mobile-nav");
+const readAllBtn = document.getElementById("read-all-btn");
+const hiddenAbout = document.getElementById("hidden-about");
 
 hamburgerBtn.addEventListener("click", handleHam);
+readAllBtn.addEventListener("click", handleReadAll);
+
+function handleReadAll() {
+  hiddenAbout.classList.toggle("hidden");
+  if (!hiddenAbout.classList.contains("hidden")) {
+    readAllBtn.textContent = "Show Less"
+  } else {
+    readAllBtn.textContent = "Read More"
+  }
+}
 
 function handleHam(e) {
   hamburgerBtn.classList.toggle("toggleMenu");
-  mobileNav.classList.toggle("hidden")
+  mobileNav.classList.toggle("hidden");
 }
